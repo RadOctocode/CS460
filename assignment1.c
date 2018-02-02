8ab330e272b34ba40c2c4effd3e8e63beaa21b24
RadOctocode <mtan2198@gmail.com>
2018-02-01 20:30:23 -0500

    can daw horizontal lines

--- assignment1.c
+++ assignment1.c
@@ -1,30 +1,40 @@
 #include <GL/glut.h>
 #include <GL/gl.h>
 
+//gcc -o foo assignment1.c -lglut -lGLU -lGL -lm 
 int clicked=0;
- 
+int x1, y1, x2, y2;
 void mouse(int bin, int state , int x , int y) {
 	if(bin == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
 		
-	    float xfloat, yfloat;
+        
+		glColor3f(1,0,0);
         
         if(!clicked){
             clicked=1;
-            glBegin(GL_POINTS);
-		    xfloat=x;
-		    yfloat=glutGet(GLUT_WINDOW_HEIGHT)-y;
-		    glColor3f(1,0,0);
-		    glVertex3f(xfloat,yfloat,0.0);
-		    glEnd();
-		    glFlush();
+            x1=x;
+		    y1=glutGet(GLUT_WINDOW_HEIGHT)-y;
         }
 
         else{
             clicked=0;
+            x2=x;
+            y2=y;
+            for(int i=x1,j=y1;i<=x2;i++){
+                glBegin(GL_POINTS);
+                x1=x;
+		        y1=glutGet(GLUT_WINDOW_HEIGHT)-y;
+                glVertex3f(i,j,0.0);
+                printf("x-%d y-%d",i,j);
+		        glEnd();
+	            glFlush(); 
+            
+            }
         
         
         
         }
+        
 
 	}
 }
