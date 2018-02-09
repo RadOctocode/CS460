430fe48eb3718dc8a72d03b00500a34834b0c534
RadOctocode <mtan2198@gmail.com>
2018-02-09 00:22:39 -0500

    works 360

--- assignment1.cpp
+++ assignment1.cpp
@@ -18,21 +18,26 @@ void draw_Line(int start_x,int start_y,int end_x,int end_y){
 
         float change_y=end_y-start_y;
         float change_x=end_x-start_x;
-        float change_error=0;
+        float change_errorx=0;
+        float change_errory=0;
         float myError=0;
         bool vertical=false;
         int myY=start_y;
+        int myX=start_x;
+        if(change_y!=0){
+
+            change_errory=float_abs(change_x/change_y);
+        }//prevents horizontal y things
 
         if(change_x!=0){
-            change_error=float_abs(change_y/change_x);
+            change_errorx=float_abs(change_y/change_x);
         }
         else{
             vertical=true;
 
         }
 
-
-        if(change_error<1 && !vertical){
+        if(change_errorx<1 && !vertical){
            
             if(change_x > 0){
 
@@ -40,7 +45,7 @@ void draw_Line(int start_x,int start_y,int end_x,int end_y){
                 for(int myX=start_x;myX<end_x;myX++){
 
                     glVertex2i(myX,myY);
-                    myError=change_error+myError;
+                    myError=change_errorx+myError;
 
                     while(myError>=0.5){
                         myY=myY+(change_y/float_abs(change_y));
@@ -58,7 +63,7 @@ void draw_Line(int start_x,int start_y,int end_x,int end_y){
                 for(int myX=start_x;myX>end_x;myX--){
 
                     glVertex2i(myX,myY);
-                    myError=change_error+myError;
+                    myError=change_errorx+myError;
 
                     while(myError>=0.5){
                         myY=myY+(change_y/float_abs(change_y));
@@ -71,12 +76,45 @@ void draw_Line(int start_x,int start_y,int end_x,int end_y){
 
 
             }//if its a negative 
-        } //if the slope is smaller than 1 and they are not vertical
-        else if(hange_error>1 && !vertical){
-            std::cout<<"hi\n";
+        } //if the slope is smaller than 1 and they are not vertical plot using x
+        else {
+            if(change_y > 0){
+
+                for(int myY=start_y;myY<end_y;myY++){
+
+                    glVertex2i(myX,myY);
+                    myError=change_errory+myError;
+
+                    while(myError>=0.5){
+                        myX=myX+(change_x/float_abs(change_x));
+                        myError=myError-1;
+                    }
+
+
+                }
+
+            }//if the change in y is positive 
+            
+            else{
+
+                for(int myY=start_y;myY>end_y;myY--){
+
+                    glVertex2i(myX,myY);
+                    myError=change_errory+myError;
+
+                    while(myError>=0.5){
+                        myX=myX+(change_x/float_abs(change_x));
+                        myError=myError-1;
+                    }
+
+
+                }
+
+
+            }
             
 
-        }
+        }//otherwise plot using y
 
        
         
